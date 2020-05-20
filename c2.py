122431b46542f212ccd36b3b5e6f2ff8101cc76b
Daniel de Souza Carvalho <danielscarvalho@gmail.com>
2020-05-19 21:05:12 -0300

    Add files via upload

--- c2.py
+++ c2.py
@@ -7,10 +7,29 @@ with open("pascal-keys.txt","r") as pkeys:
     pascal_keys = pkeys.read().split()
 
 with open("p1.pas","r") as pkeys:
-    pascal_code = pkeys.read().lower().split()
+    pascal_code = pkeys.read().lower().split(' ')
 
-print(pascal_keys)
-print(pascal_code)
+lexemas=[]
+
+integerPattern =  r"^[0-9]$"
+floatPattern =  r"^[0-9].[0-9]$"
+
+for token in pascal_code:
+    if token in pascal_keys:
+       lexemas.append([token,"key word"])
+    elif re.match(integerPattern, token):
+       lexemas.append([token,"integer"])
+    elif re.match(floatPattern, token):
+       lexemas.append([token,"float"])    
+    else:
+       lexemas.append([token,"unknow"])
+
+#print(pascal_keys)
+#print(pascal_code)
+print(lexemas)
+
+with open("tokens.txt","w") as lexemas_file:
+    lexemas_file.write(str(lexemas))
 
 # 1. ler arquivo
 # 2. quebrar em tokens
@@ -23,3 +42,5 @@ print(pascal_code)
 # operações (+, -, *, ^, =, :=)
 
 # Lex - Parser >>> WEB Scraping
+
+# TODO: Terminar...
